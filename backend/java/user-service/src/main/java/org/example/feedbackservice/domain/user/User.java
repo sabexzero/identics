e7a533bc0cb6cd73ca037b985a90329b398506a4
skendli <basek0178@gmail.com>
2024-09-19 02:20:48 +0300

    1)Добавленая заготовка для валидации данных о пользователе на уровне классов запроса. 2)Убраны аннотации валидации из класса User.

--- backend/java/user-service/src/main/java/org/example/feedbackservice/domain/user/User.java
+++ backend/java/user-service/src/main/java/org/example/feedbackservice/domain/user/User.java
@@ -4,9 +4,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
@@ -22,23 +19,15 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotBlank
-    @Size(min = 2, max = 50)
     private String name;
 
-    @NotBlank
-    @Size(min = 2, max = 50)
     private String surname;
 
-    @Size(max = 50)
     private String patronymic;
 
-    @NotNull
     private City city;
 
-    @NotNull//
     private EducationalOrganization educationalOrganization;
 
-    @NotNull
     private UserType type;
 }
--- backend/java/user-service/src/main/java/org/example/feedbackservice/web/requests/UserUpdateRequest.java
+++ backend/java/user-service/src/main/java/org/example/feedbackservice/web/requests/UserUpdateRequest.java
@@ -1,5 +1,7 @@
 package org.example.feedbackservice.web.requests;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import org.example.feedbackservice.domain.user.User;
 import org.example.feedbackservice.domain.user.UserType;
 import org.example.feedbackservice.domain.geo.City;
@@ -8,15 +10,19 @@ import java.util.Optional;
 
 public record UserUpdateRequest(
         Long id,
-
+        @NotBlank
+        @Size(min = 2, max = 50)
         String name,
+        @NotBlank
+        @Size(min = 2, max = 50)
         String surname,
+        @Size(max = 50)
         String patronymic,
 
         City city,
         EducationalOrganization educationalOrganization,
-
-        UserType type
+        @NotBlank
+        String userType
 ) {
     public User toDomain() {
         User.UserBuilder builder = User.builder()
@@ -26,7 +32,14 @@ public record UserUpdateRequest(
         Optional.ofNullable(patronymic).ifPresent(builder::patronymic);
         Optional.ofNullable(city).ifPresent(builder::city);
         Optional.ofNullable(educationalOrganization).ifPresent(builder::educationalOrganization);
-        Optional.ofNullable(type).ifPresent(builder::type);
+        Optional.ofNullable(userType).ifPresent(type -> {
+            try {
+                UserType userTypeEnum = UserType.valueOf(type.toUpperCase());
+                builder.type(userTypeEnum);
+            } catch (IllegalArgumentException e) {
+                throw new RuntimeException("Invalid user type: " + type);
+            }
+        });
         return builder.build();
     }
 }
