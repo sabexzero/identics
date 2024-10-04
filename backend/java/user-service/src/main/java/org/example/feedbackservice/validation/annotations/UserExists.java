69aa3f2bc0daadef0ba92935eb8f848bf98e08ff
skendli <basek0178@gmail.com>
2024-10-05 02:23:26 +0300

    1)Добавлена кастомная аннотация UserExists, которая вместе с кастомным валидатором UserExistsValidator проверяет данные на дефект USER_NOT_EXISTS. 2) Внедрена валидация поля id в UserUpdateRequest, в UserServiceImpl.

---
+++ backend/java/user-service/src/main/java/org/example/feedbackservice/validation/annotations/UserExists.java
@@ -0,0 +1,22 @@
+package org.example.feedbackservice.validation.annotations;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import org.example.feedbackservice.validation.UserExistsValidator;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Constraint(validatedBy = UserExistsValidator.class)
+@Target({ ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface UserExists {
+    String message() default "USER_NOT_EXISTS";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
+
