08caf3566e0d6574a8e454ee04a6294b69dd3479
Entitd <amaluteshev92@gmail.com>
2024-10-16 22:21:46 +0300

    add personalpage(not complete)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,14 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage/LoginPage';
 import MainPage from './pages/MainPage/MainPage';
 import RegistrationPage from './pages/RegistrationPage/RegistrationPage';
-// import PersonalAccount from './pages/PersonalAccount/PersonalAccount';
+import PersonalAccount from './pages/PersonalAccount/PersonalAccount';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path='/mainpage' element={<MainPage />} />
-        {/* <Route path='/profile' element={<PersonalAccount />} /> */}
+        <Route path='/profile' element={<PersonalAccount />} />
         <Route path='/login' element={<LoginPage />} />
         <Route path='/register' element={<RegistrationPage />} />
       </Routes>
---
+++ frontend/src/pages/PersonalAccount/PersonalAccount.jsx
@@ -0,0 +1,38 @@
+import './personal-account.css';
+import Button from '../../components/Button/Button';
+// import LinkButton from '../../components/LinkButton/LinkButton';
+// import Input from '../../components/Input/Input';
+import Img_man from '../../assets/images/img_man.png';
+import Search from '../../components/Search/Search';
+import Search_icon from '../../assets/images/search_icon.png';
+
+const PersonalAccount = () => {
+    return (
+        <>
+            <div className="profile-container">
+                <div className="up_bar">
+                    <h1>IDENTICS</h1>
+                    <Button isWhite = {true}>Выйти</Button>
+                </div>
+            </div>
+
+            <div className="black_block">
+                <div className="content_block">
+                    <div className="text_or_doc">
+                        <h2>Проверить оригинальность</h2>
+                        <div className="button_container">
+                            <Button isWhite = {true}>Текст</Button>
+                            <Button isWhite = {true}>Документ</Button>
+                        </div>
+                    </div>
+                    <img src={Img_man} alt="" />
+                </div>
+            </div>
+            <div className="profile-container">
+                <Search placeholder={"Поиск документа"}></Search>
+            </div>
+        </>
+    );
+};
+
+export default PersonalAccount;
